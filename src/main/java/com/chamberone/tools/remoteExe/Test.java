a7331c36820ad89a8494239588395cedf3f61c99
zhangguoping <zhangguoping>
2019-06-19 14:35:06 +0800

    initial commit

---
+++ src/main/java/com/chamberone/tools/remoteExe/Test.java
@@ -0,0 +1,20 @@
+package com.chamberone.tools.remoteExe;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        String path =
+                ".\\target\\test-classes\\org\\service\\api\\RemoteTest.class";
+        InputStream is = new FileInputStream(path);
+        byte[] b = new byte[is.available()];
+        is.read(b);
+        is.close();
+        String output = JavaClassExecuter.execute(b);
+        System.out.print("[" + output + "]");
+    }
+
+}
